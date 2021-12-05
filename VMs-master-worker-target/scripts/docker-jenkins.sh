ef994384eb07fe6e30155b89a9bfdb75c4096f81
shamal112mn <azatmurataliev@gmail.com>
2021-12-05 12:33:26 -0600

    fixed path

--- VMs-master-worker-target/scripts/docker-jenkins.sh
+++ VMs-master-worker-target/scripts/docker-jenkins.sh
@@ -3,26 +3,30 @@
 
 docker run -d --name myjenkins -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts-jdk11
 
-# sudo usermod -aG docker jenkins # this won't work because jenkins user doesn't exit
+ 
 pathToJenkinsHome="/home/vagrant/myfolder/backups"
+
 if [ -d "$pathToJenkinsHome/jenkins_home" ]
 then 
     echo " Folder jenkins_home exist and skip the preparation."
 else
+    cd $pathToJenkinsHome
+
     if [ -f jenkins-home-tar-gzaa ]
     then
         echo "Preparing jenkins_home for swapping"
-        cd $pathToJenkinsHome
+        
         cat  jenkins-home-tar-gz* > jenkins_home.tar.gz
         tar -xvzf jenkins_home.tar.gz 
         rm jenkins_home.tar.gz
         rm jenkins-home-tar*
         rm split-backup.sh
+
         echo "Folder jenkins_home is prepared"
     else
         echo "****************************"
         echo
-        echo "Jenkins Archive file is missing or broken. "
+        echo "Jenkins Archive files is missing or broken. "
         echo "Folder jenkins_home will be not configure for you."
         echo "Please, Reclone project or configure manually"
         echo
@@ -34,7 +38,7 @@ else
 
 fi
 echo "Swapping jenkins_home for running myjenkins container"
-sleep 10
+sleep 15
 sudo rm -rf /var/lib/docker/volumes/jenkins_home
 sleep 2
 sudo cp -r  /home/vagrant/myfolder/backups/jenkins_home  /var/lib/docker/volumes
@@ -53,4 +57,5 @@ echo "     password: admin"
 echo 
 echo "************************************"
 
+ 
  
